2b026f24438b2459678a2fdf2ac867a2b7c8a52e
Oscar Salas <estebansalas2502@gmail.com>
2024-04-22 11:51:14 -0600

    Cambios

--- Proyecto2.DataBase/Scripts/Script.PostDeployment.sql
+++ Proyecto2.DataBase/Scripts/Script.PostDeployment.sql
@@ -4,7 +4,7 @@
  INSERT INTO TipoUsuario VALUES('Artista',0)
  INSERT INTO TipoUsuario VALUES('Comprador',0)
 
- INSERT INTO CategoriaObra VALUES('Pintura,',0)
- INSERT INTO CategoriaObra VALUES('Escultura,',0)
- INSERT INTO CategoriaObra VALUES('Fotografía,',0)
+ INSERT INTO CategoriaObra VALUES('Pintura',0)
+ INSERT INTO CategoriaObra VALUES('Escultura',0)
+ INSERT INTO CategoriaObra VALUES('Fotografía',0)
 
