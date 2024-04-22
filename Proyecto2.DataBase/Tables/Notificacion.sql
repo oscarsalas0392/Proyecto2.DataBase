a7ed57a982e840aaadba9fceaaaed935a5cba57e
Oscar Salas <estebansalas2502@gmail.com>
2024-04-22 11:41:35 -0600

    Cambios en notificacion

--- Proyecto2.DataBase/Tables/Notificacion.sql
+++ Proyecto2.DataBase/Tables/Notificacion.sql
@@ -1,12 +1,15 @@
 ﻿CREATE TABLE [dbo].[Notificacion]
 (
 	[Id] INT NOT NULL IDENTITY(1,1),
-	[Correo] VARCHAR(50) NOT NULL,
+	[Usuario] INT NOT NULL,
 	[Titulo] VARCHAR(100) NOT NULL,
 	[Descripcion] VARCHAR(500) NOT NULL,
 	[Enviado] BIT NOT NULL,
+	[Leido] BIT NOT NULL,
+	[PorApp] BIT NOT NULL,
 	[Error] VARCHAR(500),
 	[Eliminado] BIT NOT NULL,
 	CONSTRAINT PK_Notificacion PRIMARY KEY(Id),
+	CONSTRAINT FK_Notificacion_Usuario FOREIGN KEY(Usuario) REFERENCES Usuario(Id)
 
 )
