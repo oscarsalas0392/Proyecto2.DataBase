77ebd7504387a23becef4f0abce5471bbef91eec
Oscar Salas <estebansalas2502@gmail.com>
2024-04-23 11:49:50 -0600

    Cambios

--- Proyecto2.DataBase/Tables/Transaccion.sql
+++ Proyecto2.DataBase/Tables/Transaccion.sql
@@ -2,12 +2,13 @@
 (
 	[Id] INT NOT NULL IDENTITY(1,1),
 	[Usuario] INT NOT NULL,
-	[ObraArte] INT NOT NULL,
-	[Monto] DECIMAL(22,2),
+	[Subasta] INT NOT NULL,
+	[Oferta] INT NOT NULL,
 	[Tarjeta] VARCHAR(18) NOT NULL,
 	[Fecha] DATETIME NOT NULL,
 	[Eliminado] BIT NOT NULL,
 	CONSTRAINT PK_Transaccion PRIMARY KEY(Id),
 	CONSTRAINT FK_Transaccion_Usuario FOREIGN KEY(Usuario) REFERENCES Usuario(Id),
-	CONSTRAINT FK_Transaccion_ObraArte FOREIGN KEY(ObraArte) REFERENCES ObraArte(Id)
+	CONSTRAINT FK_Transaccion_Subasta FOREIGN KEY(Subasta) REFERENCES Subasta(Id),
+	CONSTRAINT FK_Transaccion_Oferta FOREIGN KEY(Oferta) REFERENCES Oferta(Id)
 )
